7bf9a0a1e1cc9abdd7a1c387720133f9998f7184
jameswilddev <jameswilddev@outlook.com>
2018-01-21 14:01:32 +0000

    Added a placeholder browser.

---
+++ src/browser.js
@@ -0,0 +1,108 @@
+import { h } from "picodom"
+import "./browser.css"
+
+export default {
+  view: () => <div id="browser">
+    <div class="section">
+      <div class="header">
+        <div class="button">＋&#xFE0E;</div>
+        <div class="name"><div class="icon">►&#xFE0E;</div>Abc</div>
+      </div>
+      <div class="items">
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="active item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="header">
+        <div class="icon">►&#xFE0E;</div>
+        <div class="button">＋&#xFE0E;</div>
+        <div class="name">Abc</div>
+      </div>
+      <div class="items">
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="active item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="header">
+        <div class="icon">►&#xFE0E;</div>
+        <div class="button">＋&#xFE0E;</div>
+        <div class="name">Abc</div>
+      </div>
+      <div class="items">
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+        <div class="item">
+          <div class="button">❌&#xFE0E;</div>
+          <div class="button">🖉&#xFE0E;</div>
+          <div class="name">Item</div>
+        </div>
+      </div>
+    </div>
+  </div>
+}
--- src/view.js
+++ src/view.js
@@ -1,6 +1,8 @@
 import { h } from "picodom"
 import toolbar from "./toolbar.js"
+import browser from "./browser.js"
 
 export default () => <div>
   {toolbar.view()}
+  {browser.view()}
 </div>
